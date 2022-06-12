b27b7aa868c019833b2a337e3ecb49a29888a254
PGDimitrov <peter_gd@mail.bg>
2022-06-13 01:29:03 +0300

    Revert "Revert "Change the name to Homework_Lesson_8_Exercise_1""
    This reverts commit 2d944434b36453a848bac3bf24d903087dbe9c7b.

---
+++ src/homework_Lesson_8_Exercise_1/Rectangle.java
@@ -0,0 +1,40 @@
+package homework_Lesson_8_Exercise_1;
+
+public class Rectangle extends Shape{
+
+    double width;
+    double length;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    @Override
+    public double getArea() {
+        return width*length;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (width + length)*2;
+    }
+
+}
